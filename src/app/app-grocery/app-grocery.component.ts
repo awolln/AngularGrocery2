87fbc6a03719a701c76cfd27dcdddf8a6c482764
awolln <aweln457@gmail.com>
2023-03-31 22:18:33 -0700

    change component

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
-  styleUrls: [ './app-grocery.component.css']
+  styleUrls: [ 
+   './app-grocery.component.css'
+  ]
 })
 export class AppGroceryComponent {
   task!: string;
