c9318dc5b36a2d5ad6130d3b341e4743c0f75bf9
awolln <aweln457@gmail.com>
2023-03-31 21:45:16 -0700

    creat angulargrocery

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: [ './app-grocery.component.css']
+})
+export class AppGroceryComponent {
+  task!: string;
+  tasks: any;
+
+  onClick(){
+    
+    this.tasks.push({ name: this.task });
+    this.task = '';
+}
+
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   AppGroceryComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppGroceryComponent]
 })
 export class AppModule { }
